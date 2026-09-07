a1038edea81f32e59e53b05f6d1a6b4ac9b2ca8c
Raj Sharma <sharmaarajj5@gmail.com>
2026-09-07 09:28:25 +0000

    Day 28 - Q55 and Q56

---
+++ Day-28/q56.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Array elements are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
