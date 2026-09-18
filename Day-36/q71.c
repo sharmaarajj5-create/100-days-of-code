d455b2d57c5c1e17c9b2be722f92a0dfd346ff97
Raj Sharma <sharmaarajj5@gmail.com>
2026-09-18 17:08:50 +0000

    Day 36 - Q71 and Q72

---
+++ Day-36/q71.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int rows, cols, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Matrix is:\n");
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day-36/q72.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int rows, cols, i, j, sum = 0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
