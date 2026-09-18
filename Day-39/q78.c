00c8c75c568efc5dc993b1ac42686854b3bb95e2
Raj Sharma <sharmaarajj5@gmail.com>
2026-09-18 17:19:52 +0000

    Day 39 - Q77 and Q78

---
+++ Day-39/q78.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j, sum = 0;
+
+    printf("Enter size of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Sum of main diagonal = %d\n", sum);
+
+    return 0;
+}
